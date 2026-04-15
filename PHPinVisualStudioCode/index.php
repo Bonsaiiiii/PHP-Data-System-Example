dbb468a0b51995990303bd66fa613f7981113c6a
Caio Daniel Hames de Souza <138807198+Bonsaiiiii@users.noreply.github.com>
2026-04-15 16:20:01 -0300

    Create HTML structure and form in index.php
    Added HTML structure and form to index.php.

--- PHPinVisualStudioCode/index.php
+++ PHPinVisualStudioCode/index.php
@@ -1,1 +1,22 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <main>
+        <form action="include/formhandler.php" method="post">
+            <label for="firstname">Firstname>?</label>
+            <input id="firstname" type="text" name="firstname" placeholder="Firstname...">
+
+            
+
+</body>
