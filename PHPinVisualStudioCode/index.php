8d67c73195866337cd946ee341167290a712b169
Caio Daniel Hames de Souza <138807198+Bonsaiiiii@users.noreply.github.com>
2026-04-16 14:05:11 -0300

    Add required fields and gender selection to form

--- PHPinVisualStudioCode/index.php
+++ PHPinVisualStudioCode/index.php
@@ -13,10 +13,23 @@
 <body>
 
     <main>
-        <form action="include/formhandler.php" method="post">
-            <label for="firstname">Firstname>?</label>
-            <input id="firstname" type="text" name="firstname" placeholder="Firstname...">
+        <form action="includes/formhandler.php" method="post">
+            <label for="firstname">Firstname?</label>
+            <input required id="firstname" type="text" name="firstname" placeholder="Firstname...">
 
-            
+            <label for="lastname">Lastname?</label>
+            <input required id="lastname" type="text" name="lastname" placeholder="Lastname...">
 
+            <label for="gender">Gender?</label>
+            <select id="gender" name="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="none">None</option>
+            </select>
+
+            <button type="submit" name="submit">Submit</button>
+        </form>
+    </main>
 </body>
+
+</html>
