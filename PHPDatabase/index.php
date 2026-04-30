de30778e93d13642e09743870e4f622881b9cebd
Caio Daniel Hames de Souza <138807198+Bonsaiiiii@users.noreply.github.com>
2026-04-30 15:50:58 -0300

    Update index.php

--- PHPDatabase/index.php
+++ PHPDatabase/index.php
@@ -19,8 +19,23 @@
         <input type="password" name="pwd" placeholder="Password">
         <input type="text" name="email" placeholder="E-mail">
         <button>Signup</button>
+    </form>
 
+    <h3>Change account</h3>
 
+    <form action="includes/userupdate.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-mail">
+        <button>Update</button>
+    </form>
+
+    <h3>Update account</h3>
+
+    <form action="includes/userdelete.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Delete</button>
     </form>
 
 </body>
