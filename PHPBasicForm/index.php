011fb6fe1c2846ddead2ae9f899c7d27fd818145
unknown <caiodhames@gmail.com>
2026-05-06 13:25:07 -0300

    Version with Search

---
+++ PHPBasicForm/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <main>
+        <form action="includes/formhandler.php" method="post">
+            <label for="firstname">Firstname?</label>
+            <input required id="firstname" type="text" name="firstname" placeholder="Firstname...">
+
+            <label for="lastname">Lastname?</label>
+            <input required id="lastname" type="text" name="lastname" placeholder="Lastname...">
+
+            <label for="gender">Gender?</label>
+            <select id="gender" name="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="none">None</option>
+            </select>
+
+            <button type="submit" name="submit">Submit</button>
+        </form>
+    </main>
+</body>
+
+</html>
